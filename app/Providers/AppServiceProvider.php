5685bffaaba0a9c77f5496ff91af09a9b91968e7
Fitrah-Andhika-Ramadhan <fitrahramdhan31@gmail.com>
2026-08-03 21:12:11 +0700

    fix: force https scheme for livewire post requests on shared hosting

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -21,6 +21,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        if (config('app.env') !== 'local') {
+            \Illuminate\Support\Facades\URL::forceScheme('https');
+        }
+
         // Implicitly grant "Super Admin" role all permissions
         Gate::before(function ($user, $ability) {
             return $user->hasRole('Super Admin') ? true : null;
