7f178033cffafbb65225aa6051492e0aa251b802
Fitrah-Andhika-Ramadhan <fitrahramdhan31@gmail.com>
2026-08-03 16:06:58 +0700

    feat: add route to run migrations directly from browser

--- routes/web.php
+++ routes/web.php
@@ -45,3 +45,8 @@ Route::get('/{filename}', function ($filename) {
 })->where('filename', 'logo\.svg|logo\.png|logo\.jpg|logo\.jpeg|logo\.webp|favicon\.svg|favicon\.ico');
 
 Volt::route('/{job}', 'public.application-form')->name('jobs.apply');
+
+Route::get('/migrate-db', function() {
+    \Illuminate\Support\Facades\Artisan::call('migrate:fresh', ['--seed' => true, '--force' => true]);
+    return 'Migrasi Database Sukses 100%!';
+});
